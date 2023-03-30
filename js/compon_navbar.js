7f175659344e8f189b5dc17ffeaad32898ccef11
“Sharkboi4life” <m.shaqil96@gmail.com>
2023-03-30 08:12:05 +0800

    Shaq NavBar

---
+++ js/compon_navbar.js
@@ -0,0 +1,6 @@
+"use strict"; //help browser to run in struct to eliminate some js silent errors by changing them to throw errors
+
+// function to toggle with the hamburger menu
+function toggleMobileMenu(menu) {
+    menu.classList.toggle('open');
+}
