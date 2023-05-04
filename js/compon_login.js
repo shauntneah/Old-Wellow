5bccfc749d66621fdb96cdf292fa59a89f0eae21
“Sharkboi4life” <m.shaqil96@gmail.com>
2023-05-04 08:20:08 +0800

    Home,Login,Register

--- js/compon_login.js
+++ js/compon_login.js
@@ -1,3 +1,23 @@
 function login(username, password) {
   // code to perform login
 }
+
+
+// not used
+ // Example starter JavaScript for disabling form submissions if there are invalid fields
+ (function() {
+  'use strict';
+  // Fetch all the forms we want to apply custom Bootstrap validation styles to
+  var forms = document.querySelectorAll('.needs-validation');
+  // Loop over them and prevent submission
+  Array.prototype.slice.call(forms)
+    .forEach(function(form) {
+      form.addEventListener('submit', function(event) {
+        if (!form.checkValidity()) {
+          event.preventDefault();
+          event.stopPropagation();
+        }
+        form.classList.add('was-validated');
+      }, false);
+    });
+})();
